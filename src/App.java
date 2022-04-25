5c026d728809820899b0fb8363b632b2c8cb6e15
Andrey Bernardoni <andreybernardoni.gr041@academico.ifsul.edu.br>
2022-04-25 18:03:43 -0300

    Criação do menu

--- src/App.java
+++ src/App.java
@@ -1,5 +1,49 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        int option = 0;
+
+        do {
+
+            menu();
+
+            option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    
+                    break;
+            
+                case 2:
+                    
+                    break;
+            
+                case 3:
+                    System.out.println("Saindo...");
+                    break;
+            
+                default:
+                    System.out.println("Opção Inválida!");
+                    break;
+            }
+
+        } while (option != 3);
+
+        sc.close();
     }
+
+    public static void menu() {
+        System.out.println("1.) Cadastro ");
+        System.out.println("2.) Consultar");
+        System.out.println("3.) Sair");
+        System.out.println("\nInsira sua opção: ");
+    }
+
+    public void clear() {
+        System.out.print("033 [H033 [2J");
+    }
+
 }
